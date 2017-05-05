6430f0d61ffd85da22e26b03a9613a727f02c7dc
elisalaufer <elisa.cristina.laufer@gmail.com>
2017-05-05 09:26:26 -0300

    Add files via upload

---
+++ logicaa.php
@@ -0,0 +1,12 @@
+
+<?php
+    $tratamento = ['Dona','Samurai','Oráculo'];
+    $qualidade = ['Paladina', 'A Vossa Senhoria','O homosapiens'];
+    $lugar = ['das terras medias', 'de Araquari','de Hogwarts'];
+    $nome = "Elisinha";
+
+       $tratamento = $tratamento[rand(0,2)];
+       $qualidade  = $qualidade[rand(0,2)];
+       $lugar = $lugar[rand(0,2)];
+
+
